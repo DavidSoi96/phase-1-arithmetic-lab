5b90b1ea5eeaed0aa019fcb510683df01e1a67f5
DavidSoi96 <david.soi@student.moringaschool.com>
2025-01-19 12:50:25 +0300

    Arithmetic lab assignment

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;
+console.log(multiply);
+
+let random = Math.floor(Math.random() * 100) + 1; 
+
+let num3 = 14;
+let num4 = 10;
+
+const mod = num3 % num4;
+
+const max = Math.max(10,20,14,2);
+
+
+
+
+
+
